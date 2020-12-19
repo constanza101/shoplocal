7afffbfb90f19f759bb06645abe4b530d7d22c73
constanza diaz <cdiaz@admira.com>
2020-12-19 16:49:00 +0100

    main: adding leaflet map

---
+++ shop-local/src/views/map/Index.vue
@@ -0,0 +1,35 @@
+<script>
+  // Extensions
+  import View from '@/views/View'
+
+  // Mixins
+  import LoadSections from '@/mixins/load-sections'
+
+  export default {
+    name: 'Map',
+
+    metaInfo: { title: 'About Us' },
+
+    extends: View,
+
+    mixins: [
+      LoadSections([
+        'leaflet-map',
+        'hero-alt',
+        'about-our-product',
+        'theme-features',
+        'affiliates',
+        'newsletter',
+        'pro-features',
+        'info',
+      ]),
+    ],
+
+    props: {
+      id: {
+        type: String,
+        default: 'about',
+      },
+    },
+  }
+</script>
